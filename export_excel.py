8128b44c9ae2035310ba88c785bfb3f8975631b1
Juzo(-.-) <alexs22221@gmail.com>
2024-10-13 22:01:34 +0300

    add export data

---
+++ export_excel.py
@@ -0,0 +1,34 @@
+from sqlalchemy import select
+import pandas as pd
+
+from database.connection import SessionManager, Session
+from database.models import Weather
+
+
+def get_data_from_db(limit: int = 10):
+    query = select(Weather).order_by(Weather.timestamp.desc()).limit(limit)
+    results = []
+
+    with SessionManager(Session) as session:
+        data = session.scalars(query).all()
+    
+        df = pd.DataFrame([{
+                'id': record.id,
+                'Temperature': record.temperature,
+                'Wind speed': record.wind_speed,
+                'Wind direction': record.wind_direction,
+                'Pressure': record.pressure,
+                'Precipitation type': record.precipitation_type,
+                'Precipitation amount': record.precipitation_amount,
+                'Timestamp':record.timestamp
+                } for record in data])
+     
+    return df
+
+
+def export_to_excel(file_name: str):
+    df = get_data_from_db()
+    
+    # Экспортируем в Excel
+    df.to_excel(file_name + '.xlsx', index=False)
+    print(f"Данные экспортированы в {file_name}")
